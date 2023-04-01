ff4c112046a5b526f2eb803498b69a44e9cabd8e
Ivan Kurski <iskurskiii@gmail.com>
2023-04-01 16:26:17 +0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+import { Test } from './components';
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <Test />
+    </div>
+  );
+};
+
+export default App;
